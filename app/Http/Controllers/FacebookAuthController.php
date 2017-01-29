c2011ccc3fef80a6d6c467fc48b1db50ffabd934
Benjamin Arteaga <barteaga@pamdogs.com>
2017-01-28 23:02:44 -0400

    Testing Facebook Auth

--- app/Http/Controllers/FacebookAuthController.php
+++ app/Http/Controllers/FacebookAuthController.php
@@ -6,7 +6,9 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
- use Laravel\Socialite\Contracts\Factory as Socialite;
+// use Laravel\Socialite\Contracts\Factory as Socialite;
+use Auth;
+use Socialite;
 
 class FacebookAuthController extends Controller
 {
