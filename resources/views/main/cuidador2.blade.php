060d3ab265fdaa66666941acc7bb2461680ce123
Benjamin Arteaga <barteaga@pamdogs.com>
2017-02-27 20:07:26 -0400

    Commit

--- resources/views/main/cuidador2.blade.php
+++ resources/views/main/cuidador2.blade.php
@@ -769,7 +769,7 @@ body{
    if (document.location.protocol != 'https:') document.write('\x3Cscript src="http://musecdn2.businesscatalyst.com/scripts/4.0/jquery-1.8.3.min.js" type="text/javascript">\x3C/script>');
 </script>
   <script type="text/javascript">
-   window.jQuery || document.write('\x3Cscript src="{{ url('scripts/jquery-1.8.3.min.js?crc=209076791') }}" type="text/javascript">\x3C/script>');
+   window.jQuery || document.write('\x3Cscript src="{{ url('main/scripts/jquery-1.8.3.min.js?crc=209076791') }}" type="text/javascript">\x3C/script>');
 </script>
   <!-- Other scripts -->
   <script type="text/javascript">
