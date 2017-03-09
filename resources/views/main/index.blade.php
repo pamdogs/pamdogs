33033687d51e7f16d1386d9be593a19d1514ac55
Benjamin Arteaga <barteaga@pamdogs.com>
2017-03-09 19:13:46 -0400

    Prueba autocompletado de Google Maps.

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -618,5 +618,15 @@ $(window).load(function(){
 });
 </script>
 
+<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=AIzaSyDWBFZiDcF4U80G-iQRraEvhetVcTBlxYE&libraries=places"></script>
+<script type="text/javascript">
+    google.maps.event.addDomListener(window, 'load', function () {
+        var places = new google.maps.places.Autocomplete(document.getElementById('widgetu969_input'));
+        google.maps.event.addListener(places, 'place_changed', function () {
+
+        });
+    });
+</script>
+
    </body>
 </html>
