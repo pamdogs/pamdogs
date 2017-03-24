c4484785513436d830db801a874e53dec546a111
Benjamin Arteaga <barteaga@pamdogs.com>
2017-03-24 09:06:02 -0400

    Test login con Facebook

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -122,7 +122,8 @@ class AuthController extends Controller
         } catch (Exception $e) {
             dd($e);
         }*/
-        dd($request->header->get('referer'));
+        $request->header->get('referer');
+        dd($request->header);
 
         /*Auth::login($authUser);
         return redirect('registro/cliente#');*/
