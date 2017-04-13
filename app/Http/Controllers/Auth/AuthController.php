46de55f400dbb153900f4067f7dac141846c475c
Benjamin Arteaga <barteaga@pamdogs.com>
2017-04-13 13:43:08 -0400

    Arreglando Facebook Login

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -75,13 +75,13 @@ class AuthController extends Controller
         return view('main.iniciar-sesion');
     }
 
-    public function postLogin(Request $request)
+    /*public function postLogin(Request $request)
     {
         if (!Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
             return response()->json(['error' => 'E-Mail o contraseña invalidos'],422);
         }
         return response()->json();
-    }
+    }*/
 
 
 
