ac9f1d7eff52c468a58bbf1948076d75b47ea401
vagrant <vagrant@homestead>
2017-05-29 18:39:53 +0000

    Agregado nuevo menu. [add] js/custom.js css/custom.css

--- resources/views/main/template.blade.php
+++ resources/views/main/template.blade.php
@@ -15,8 +15,9 @@
     <link href="{{ url('css/bootstrap.min.css') }}" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="{{ url('css/business-frontpage.css') }}" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
+    <link href="{{ url('css/business-frontpage.css') }}" rel="stylesheet">
+    <link href="{{ url('css/custom.css') }}" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -29,62 +30,79 @@
 
 <body style="background-color:#f1f1f1">
 
-    @section('menubar')
-    <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" style="color:black">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <img src="img/img5.png" width="136" height="36"></a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li>
-                        <a href="#">Inicio</a>
-                    </li>
-                    <li>
-                        <a href="{{ route('registro.index') }}">Registrate</a>
-                    </li>
-                    <li>
-                        <a href="#">Iniciar Sesion</a>
-                    </li>
-                    <li>
-                        <a href="#">Contacto</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
+    @section('sidebarnav')
+      <div id="mySidenav" class="sidenav">
+       <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+       <a href="#">Inicio</a>
+       <a href="#">Regístrate</a>
+       <a href="#">Iniciar Sesión</a>
+       <a href="#">Contacto</a>
+      </div>
+
     @show
+    <div id="main">
+      @section('menubar')
+      <!-- Navigation -->
+      <nav id="nav-top" class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+          <div class="container-fluid">
+              <!-- Brand and toggle get grouped for better mobile display -->
+              <!--<div class="navbar-header">
+                  <img src="img/img5.png" width="136" height="36"></a>
+              </div>-->
+              <!-- Collect the nav links, forms, and other content for toggling -->
+              <!--<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">-->
 
-    @yield('content')
+                  <ul class="nav navbar-nav">
+                    <img id="logo" src="img/img5.png"></a>
+                      <!--<li>
+                          <a href="#">Inicio</a>
+                      </li>
+                      <li>
+                          <a href="{{ route('registro.index') }}">Registrate</a>
+                      </li>
+                      <li>
+                          <a href="#">Iniciar Sesion</a>
+                      </li>
+                      <li>
+                          <a href="#">Contacto</a>
+                      </li>-->
+                  </ul>
 
-    @section('footer')
-    <!-- Footer -->
-    <footer>
-        <div class="row">
-            <div class="col-lg-12">
-                <p>Copyright &copy; Pamdogs 2017</p>
-            </div>
-        </div>
-        <!-- /.row -->
-    </footer>
-    @show
+                  <ul class="nav navbar-nav navbar-right">
+                    <li class="user-info"><span><b>José Requena</b> <img class="avatar-menu" src="{{ url('img/user-39x39.png') }}" alt="user-avatar"></span></li>
+                    <li><span id="button-menu" class="glyphicon glyphicon-menu-hamburger" onclick="openNav()"></span></li>
+                  </ul>
+              <!--</div>-->
+              <!-- /.navbar-collapse -->
+          </div>
+          <!-- /.container -->
+      </nav>
+      @show
 
+
+      @yield('content')
+
+
+      @section('footer')
+      <!-- Footer -->
+      <footer>
+          <div class="row">
+              <div class="col-lg-12">
+                  <p>Copyright &copy; Pamdogs 2017</p>
+              </div>
+          </div>
+          <!-- /.row -->
+      </footer>
+      @show
+    </div>
     <!-- jQuery -->
     <script src="{{ url('js/jquery.js') }}"></script>
 
     <!-- Bootstrap Core JavaScript -->
     <script src="{{ url('js/bootstrap.min.js') }}"></script>
+
+    <!-- Custom -->
+    <script src="{{ url('js/custom.js') }}"></script>
 
 </body>
 
