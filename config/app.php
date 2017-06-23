1e8806cbe874e162d11ac288e3250b905d0154f2
Benjamin Arteaga <benjaminjarteagag@gmail.com>
2017-06-23 00:25:45 -0400

    Limpiado el repositorio, instrucciones actualizadas para instalarlo.

--- config/app.php
+++ config/app.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost/pamdogs'),
+    'url' => env('APP_URL', 'http://localhost:8000/'),
 
     /*
     |--------------------------------------------------------------------------
